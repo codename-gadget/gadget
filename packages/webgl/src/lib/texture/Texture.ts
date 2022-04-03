6643991459155f76e82a1e871b47e2f1fb244603
johh <code@johh.net>
2022-04-03 15:53:45 +0200

    refactor(webgl): support multi-level texture pixel upload
    BREAKING CHANGE: The method signature of Texture.uploadPixels changed.

--- packages/webgl/src/lib/texture/Texture.ts
+++ packages/webgl/src/lib/texture/Texture.ts
@@ -93,38 +93,63 @@ export default class Texture extends ContextConsumer {
 	/**
 	 * Uploads an array of pixels to the GPU.
 	 *
-	 * @param pixels - The pixel data.
-	 * @param type - The formate the pixel data is in.
-	 * @param level - The mipmap level to upload to. @defaultValue `0`
+	 * @param pixelsPerLevel - An object containing the pixel data per level.
+	 * @param type - The format the pixel data is in.
+	 * @example Uploading a 2x2px red/black checkerboard texture and a 1x1px dark red mipmap.
+	 * ```typescript
+	 * import { Texture, BufferDataType } from '@gdgt/webgl';
+	 *
+	 *
+	 * const myTexture = new Texture( {
+	 *     width: 2,
+	 *     height: 2,
+	 *     levels: 2,
+	 * } );
+	 *
+	 * await myTexture.uploadPixels(
+	 *     {
+	 *         0: new Uint8Array([
+	 *             255, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 255, 0, 0, 255,
+	 *         ]),
+	 *         1: new Uint8Array([128, 0, 0, 255]),
+	 *     },
+	 *     BufferDataType.unsignedByte,
+	 * );
+	 * ```
 	 */
 	public async uploadPixels(
-		pixels: ArrayBufferView,
+		pixelsPerLevel: { [level: number]: ArrayBufferView },
 		type: BufferDataType | TextureDataType,
-		level = 0,
 	): Promise<void> {
 		await this.ready;
 
 		const {
 			gl, texture, format, width, height,
 		} = this;
-		const levelWidth = resolutionForLevel( width, level );
-		const levelHeight = resolutionForLevel( height, level );
-
-		// TODO: sanity checks
 
 		gl.bindTexture( gl.TEXTURE_2D, texture );
-		gl.texSubImage2D(
-			gl.TEXTURE_2D,
-			level,
-			0,
-			0,
-			levelWidth,
-			levelHeight,
-			format,
-			type,
-			pixels,
-			0,
-		);
+
+		Object.entries( pixelsPerLevel ).forEach( ([_level, srcData]) => {
+			const level = parseInt( _level, 10 );
+			const levelWidth = resolutionForLevel( width, level );
+			const levelHeight = resolutionForLevel( height, level );
+
+			// TODO: sanity checks
+
+			gl.texSubImage2D(
+				gl.TEXTURE_2D,
+				level,
+				0,
+				0,
+				levelWidth,
+				levelHeight,
+				format,
+				type,
+				srcData,
+				0,
+			);
+		} );
+
 		gl.bindTexture( gl.TEXTURE_2D, null );
 	}
 
