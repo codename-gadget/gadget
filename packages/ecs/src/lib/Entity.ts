fc538baf54dad56b42a4f6ac8c85b8895528c75c
johh <code@johh.net>
2022-06-23 20:47:07 +0200

    docs(ecs): reformat remarks

--- packages/ecs/src/lib/Entity.ts
+++ packages/ecs/src/lib/Entity.ts
@@ -202,6 +202,9 @@ export default class Entity {
 	/**
 	 * Registers a given callback to be run upon mutation to given component.
 	 *
+	 * "Mutation" in this case means, that the value of a component changed.
+	 * Addition and removal of a component is not considered a mutation.
+	 *
 	 * @remarks Note, that since mutations are flagged while reading the observed value, the callback
 	 * is likely going to run __before__ the actual change takes place. This can be worked around by
 	 * running any code accessing the updated value inside a microtask:
@@ -213,8 +216,6 @@ export default class Entity {
 	 *  } );
 	 * } );
 	 * ```
-	 * @remarks "Mutation" means, that the value of a component changed.
-	 * Addition and removal of a component is not considered a mutation.
 	 * @param declaration - The component to observe.
 	 * @param callback - The callback to run on mutation.
 	 * @returns A removal callback, that, if run, removes the mutation observer.
