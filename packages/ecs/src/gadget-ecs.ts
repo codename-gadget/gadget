4804a39ec467c496e2b304828b75000b0abc0964
johh <code@johh.net>
2022-09-12 18:35:28 +0200

    feat(ecs): export defaultWorld

--- packages/ecs/src/gadget-ecs.ts
+++ packages/ecs/src/gadget-ecs.ts
@@ -1,4 +1,5 @@
 export { default as World } from './lib/World';
+export { default as defaultWorld } from './lib/defaultWorld';
 export { default as Entity } from './lib/Entity';
 export { default as Query, QueryProps, QueryResult } from './lib/Query';
 export { default as declareComponent, ComponentDeclaration } from './lib/Component';
